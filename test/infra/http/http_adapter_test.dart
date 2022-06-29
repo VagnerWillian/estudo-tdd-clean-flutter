aaeab5909eb3a90fec16a26099aab28da3bb9bfa
Vagner Willian <tec_vagner.ti@outlook.com>
2022-06-29 01:41:02 -0300

    ensure HttpAdapter calls post with correct body

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -8,12 +8,12 @@ class ClientSpy extends Mock implements Client{}
 class HttpAdapter{
   final Client client;
   HttpAdapter(this.client);
-  Future<void> request({required String url, required String method})async{
+  Future<void> request({required String url, required String method, Map body = const {}})async{
     final headers = {
       "content-type": "application/json",
       "accept": "application/json",
     };
-    await client.post(Uri.parse(url), headers: headers);
+    await client.post(Uri.parse(url), headers: headers, body: body);
   }
 }
 
@@ -24,6 +24,7 @@ void main() {
   late final HttpAdapter sut;
   late final String url;
   late final Map<String, String> headers;
+  late final Map<String, String> body;
 
   setUp(() {
     //Arrange
@@ -34,17 +35,20 @@ void main() {
       "content-type": "application/json",
       "accept": "application/json",
     };
+    body = {
+      "any_key": "any_value"
+    };
   });
 
     group('post', () {
       test('should call post with correct values', () async {
-        when(() => client.post(Uri.parse(url), headers: headers)).thenAnswer((_) async => Response('body', 401));
+        when(() => client.post(Uri.parse(url), headers: headers, body: body)).thenAnswer((_) async => Response('body', 401));
 
         //Act
-        await sut.request(url: url, method: 'post');
+        await sut.request(url: url, method: 'post', body: body);
 
         //Expected
-        verify(() => client.post(Uri.parse(url), headers: headers));
+        verify(() => client.post(Uri.parse(url), headers: headers, body: body));
       });
     });
 }
