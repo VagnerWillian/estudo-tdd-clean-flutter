7b074e68110c7b24de0452fce8888a6b61da3a38
Vagner Willian <tec_vagner.ti@outlook.com>
2022-06-30 22:28:54 -0300

    make LoginPage presenter auth of future

--- lib/ui/pages/login/login_presenter.dart
+++ lib/ui/pages/login/login_presenter.dart
@@ -1,12 +1,12 @@
 abstract class LoginPresenter{
   Stream<String?> get emailErrorStream;
   Stream<String?> get passwordErrorStream;
+  Stream<String?> get mainErrorStream;
   Stream<bool> get enableButtonStream;
   Stream<bool> get visibleLoadingStream;
-  Stream<String?> get mainErrorStream;
 
   void validateEmail(String email);
   void validatePassword(String pass);
-  void auth();
+  Future<void> auth();
   void dispose();
 }
